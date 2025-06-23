4c58d711ad77da8b4cf2745c75a315e15156a474
udacchi <takashi.19740727@gmail.com>
2025-06-24 00:48:42 +0900

    register.blade記述

---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth/register.css')}}">
+@endsection
+
+@section('content')
+<div class="register-form">
+  <h2 class="register-form__heading content__heading">会員登録</h2>
+  <div class="register-form__inner">
+    <form class="register-form__form" action="/register" method="post">
+      @csrf
+      <div class="register-form__group">
+        <label class="register-form__label" for="name">ユーザー名</label>
+        <input class="register-form__input" type="text" name="name" id="name">
+        <p class="register-form__error-message">
+          @error('name')
+          {{ $message }}
+          @enderror
+        </p>
+      </div>
+      <div class="register-form__group">
+        <label class="register-form__label" for="email">メールアドレス</label>
+        <input class="register-form__input" type="email" name="email" id="email">
+        <p class="register-form__error-message">
+          @error('email')
+          {{ $message }}
+          @enderror
+        </p>
+      </div>
+      <div class="register-form__group">
+        <label class="register-form__label" for="password">パスワード</label>
+        <input class="register-form__input" type="password" name="password" id="password">
+        <p class="register-form__error-message">
+          @error('password')
+          {{ $message }}
+          @enderror
+        </p>
+      </div>
+      <div class="register-form__group">
+        <label class="register-form__label" for="password">確認用パスワード</label>
+        <input class="register-form__input" type="password" name="password_confirmation" id="password_confirmation">
+        <p class="register-form__error-message">
+          @error('password_confirmation')
+          {{ $message }}
+          @enderror
+        </p>
+      </div>
+      <input class="register-form__btn btn" type="submit" value="登録する">
+      <div class="register-form__link">
+        <a href="/login">ログインはこちら</a>
+      </div>
+    </form>
+  </div>
+</div>
+@endsection
