cbbefd2c709fd6451e07378b51d202e174f02cf0
udacchi <takashi.19740727@gmail.com>
2025-06-26 20:07:50 +0900

    h2タグの位置変更

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -6,8 +6,8 @@
 
 @section('content')
 <div class="register-form">
-  <h2 class="register-form__heading content__heading">会員登録</h2>
   <div class="register-form__inner">
+    <h2 class="register-form__heading content__heading">会員登録</h2>
     <form class="register-form__form" action="/register" method="post">
       @csrf
       <div class="register-form__group">
