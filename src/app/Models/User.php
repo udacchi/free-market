ee1d687f02e138f480f4292a44b69233753821cc
udacchi <takashi.19740727@gmail.com>
2025-07-29 23:15:50 +0900

    マイリストの再実装

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -30,4 +30,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function likedItems()
+    {
+        return $this->belongsToMany(Item::class, 'likes')->withTimestamps();
+    }
 }
