95d2e789a76e30cc43879767957eecbe89a39fce
udacchi <takashi.19740727@gmail.com>
2025-08-13 00:50:07 +0900

    商品情報にブランド追加

--- src/database/seeders/ItemSeeder.php
+++ src/database/seeders/ItemSeeder.php
@@ -19,6 +19,7 @@ class ItemSeeder extends Seeder
         DB::table('items')->insert([
             [
                 'name' => '腕時計',
+                'brand' => 'Armani',
                 'price' => '15000',
                 'description' => 'スタイリッシュなデザインのメンズ腕時計',
                 'image_path' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Armani+Mens+Clock.jpg',
@@ -29,6 +30,7 @@ class ItemSeeder extends Seeder
             ],
             [
                 'name' => 'HDD',
+                'brand' => '-',
                 'price' => '5000',
                 'description' => '高速で信頼性の高いハードディスク',
                 'image_path' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/HDD+Hard+Disk.jpg',
@@ -39,6 +41,7 @@ class ItemSeeder extends Seeder
             ],
             [
                 'name' => '玉ねぎ３束',
+                'brand' => '-',
                 'price' => '300',
                 'description' => '新鮮な玉ねぎ３束のセット',
                 'image_path' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/iLoveIMG+d.jpg',
@@ -49,6 +52,7 @@ class ItemSeeder extends Seeder
             ],
             [
                 'name' => '革靴',
+                'brand' => '-',
                 'price' => '4000',
                 'description' => 'クラシックなデザインの革靴',
                 'image_path' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Leather+Shoes+Product+Photo.jpg',
@@ -59,6 +63,7 @@ class ItemSeeder extends Seeder
             ],
             [
                 'name' => 'ノートPC',
+                'brand' => '-',
                 'price' => '45000',
                 'description' => '高性能なノートパソコン',
                 'image_path' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Living+Room+Laptop.jpg',
@@ -69,6 +74,7 @@ class ItemSeeder extends Seeder
             ],
             [
                 'name' => 'マイク',
+                'brand' => 'MAXIM',
                 'price' => '8000',
                 'description' => '高音質のレコーディング用マイク',
                 'image_path' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Music+Mic+4632231.jpg',
@@ -79,6 +85,7 @@ class ItemSeeder extends Seeder
             ],
             [
                 'name' => 'ショルダーバッグ',
+                'brand' => 'NINE WEST',
                 'price' => '3500',
                 'description' => 'おしゃれなショルダーバッグ',
                 'image_path' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Purse+fashion+pocket.jpg',
@@ -89,6 +96,7 @@ class ItemSeeder extends Seeder
             ],
             [
                 'name' => 'タンブラー',
+                'brand' => '-',
                 'price' => '500',
                 'description' => '使いやすいタンブラー',
                 'image_path' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Tumbler+souvenir.jpg',
@@ -99,6 +107,7 @@ class ItemSeeder extends Seeder
             ],
             [
                 'name' => 'コーヒーミル',
+                'brand' => '-',
                 'price' => '4000',
                 'description' => '手動のコヒーミル',
                 'image_path' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Waitress+with+Coffee+Grinder.jpg',
@@ -109,6 +118,7 @@ class ItemSeeder extends Seeder
             ],
             [
                 'name' => 'メイクセット',
+                'brand' => '-',
                 'price' => '2500',
                 'description' => '便利なメイクアップセット',
                 'image_path' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/%E5%A4%96%E5%87%BA%E3%83%A1%E3%82%A4%E3%82%AF%E3%82%A2%E3%83%83%E3%83%95%E3%82%9A%E3%82%BB%E3%83%83%E3%83%88.jpg',
